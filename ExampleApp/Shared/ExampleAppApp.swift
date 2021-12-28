092d0d02b248cdb48a9be149a372fc31b7bcdeb1
RachelKrimalovski3988 <96765337+RachelKrimalovski3988@users.noreply.github.com>
2021-12-28 23:15:15 +0200

    add files

---
+++ ExampleApp/Shared/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  Shared
+//
+//  Created by Rachel Krimalovski on 28/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
